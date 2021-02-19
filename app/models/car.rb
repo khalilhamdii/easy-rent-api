920eb1dc4f87d71381ab0da45cd8d350a8258c0e
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-19 01:43:24 +0100

    Add validation to car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,13 @@
 class Car < ApplicationRecord
+  validates :model, presence: true
+  validates :color, presence: true
+  validates :bodyStyle, presence: true
+  validates :pricePerDay, presence: true
+  validates :doors, presence: true
+  validates :luggages, presence: true
+  validates :seats, presence: true
+  validates :emissionClass, presence: true
+  validates :transmission, presence: true
+  validates :engine, presence: true
+  validates :rentDeposit, presence: true
 end
