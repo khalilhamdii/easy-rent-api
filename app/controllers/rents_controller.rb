c390911abd4fc7f471120ef8b542aefccef243dd
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-19 23:11:00 +0100

    Setup rents routes and fix errors

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,2 +1,25 @@
 class RentsController < ApplicationController
+  before_action :set_user
+
+  # GET /users/:user_id/rents
+  def index
+    json_response(@user.rents)
+  end
+
+  # POST /users/:user_id/rents
+  def create
+    @user.rents.create!(rent_params)
+    json_response(@user, :created)
+  end
+
+  private
+
+  def rent_params
+    params.permit(:userName, :model, :status, :pickUpDate, :pickUpTime, :returnDate, :returnTime, :location,
+                  :pricePerDay)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
