24aea4f4e6ab4b71ffc910bda798aeadf26b7290
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-19 01:37:30 +0100

    Generate Car model

---
+++ db/migrate/20210219003636_create_cars.rb
@@ -0,0 +1,19 @@
+class CreateCars < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cars do |t|
+      t.string :model
+      t.string :color
+      t.string :bodyStyle
+      t.string :pricePerDay
+      t.string :doors
+      t.string :luggages
+      t.string :seats
+      t.string :emissionClass
+      t.string :transmission
+      t.string :engine
+      t.string :rentDeposit
+
+      t.timestamps
+    end
+  end
+end
