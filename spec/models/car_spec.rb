161e54027c600a620046f648887296d43707538a
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-23 19:17:57 +0100

    Add test cases to Car model

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:model) }
+  it { should validate_presence_of(:color) }
+  it { should validate_presence_of(:bodyStyle) }
+  it { should validate_presence_of(:pricePerDay) }
+  it { should validate_presence_of(:doors) }
+  it { should validate_presence_of(:luggages) }
+  it { should validate_presence_of(:seats) }
+  it { should validate_presence_of(:emissionsClass) }
+  it { should validate_presence_of(:transmission) }
+  it { should validate_presence_of(:engine) }
+  it { should validate_presence_of(:rentDeposit) }
+  it { is_expected.to validate_attached_of(:carImg) }
 end
