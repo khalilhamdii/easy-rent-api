8044415ddef6a9d9426689094c5911aa8f08b1d8
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-23 20:18:26 +0100

    Add test cases to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :userName, presence: true, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true
 
-  has_many :rents
+  has_many :rents, dependent: :destroy
   def as_json(options = {})
     super(options.merge({ except: %i[password_digest email created_at updated_at] }))
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_user) do
+    User.new(userName: 'User',
+             email: 'user@user.com',
+             password_digest: BCrypt::Password.create('admin'),
+             role: 'USER')
+  end
+  let(:invalid_user) do
+    User.new(userName: 'ad',
+             email: 'email@email.com',
+             password_digest: BCrypt::Password.create('admin'),
+             role: 'USER')
+  end
+  it { should have_many(:rents).dependent(:destroy) }
+  it { should validate_presence_of(:userName) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { expect(valid_user).to be_valid }
+  it { expect(invalid_user).not_to be_valid }
 end
