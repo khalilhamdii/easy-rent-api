749bba29016b0cbc54d565e503ef927a4b4778fb
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-25 03:24:16 +0100

    Change rails cors to heroku link only

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,14 +16,14 @@
 # end
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:3000'
+  # allow do
+  #   origins 'http://localhost:3000'
 
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: true
-  end
+  #   resource '*',
+  #            headers: :any,
+  #            methods: %i[get post put patch delete options head],
+  #            credentials: true
+  # end
 
   allow do
     origins 'https://easyrent-rms.herokuapp.com/'
